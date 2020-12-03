89aa40890c06e160e9087aa5924896075f5ffdae
heef87 <heef159@163.com>
2020-12-03 10:32:53 +0800

    更新htpa3232库

--- libhtpa3232/src/main/jni/htpa3232_API.h
+++ libhtpa3232/src/main/jni/htpa3232_API.h
@@ -116,7 +116,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 #define ELOFFSET 1024			//start address of el. Offset
 #define ELAMOUNT 256
 #define ELAMOUNTHALF 128
-#define StackSize 16			//must be choosen by the user!
+#define StackSize 8			//must be choosen by the user!
 #define PTATSTARTADSRESS 1282
 #define VDDADDRESS 1280
 
@@ -129,56 +129,21 @@ void htpa3232_measure(int fd,int *htpa3232To);
 
 #define MAXNROFDEFECTS  20
 #define ReadToFromTable
-#ifdef ReadToFromTable
-		//#define HTPA32x32dL2_1HiSiF5_0
-		//#define HTPA32x32dL2_1HiSiF5_0_withSiFilter
-		//#define HTPA32x32dL3_6HiSi
-		//#define HTPA32x32dL3_6HiSi_Rev1
-		//#define HTPA32x32dL7_0HiSi
-		//#define HTPA32x32dL5_0HiGeF7_7
-		//#define HTPA32x32dL1_6HiGe
-		//#define HTPA32x32dR1L1_6HiGe_Gain3k3
-		//#define HTPA32x32dR1L2_1SiF5_0_N2
-		//#define HTPA32x32dL2_1HiSiF5_0_Gain3k3			//is used for SensorRev. 1	
-		//#define HTPA32x32dR1L1k8_0k7HiGe_Bodo		
-		//#define HTPA32x32dR1L2_1HiSiF5_0_Precise	
-		//#define HTPA32x32dR1L2_1HiSiF5_0_Gain3k3_Extended
-		//#define HTPA32x32dR1L2_85Hi_Gain3k3		
-		//#define HTPA32x32dR1L3_6HiSi_Rev1_Gain3k3
-		#define HTPA32x32dR1L5_0HiGeF7_7_Gain3k3	
-		//#define HTPA32x32dR1L5_0HiGeF7_7_Gain3k3_TaExtended
-		//#define HTPA32x32dR1L7_0HiSi_Gain3k3
-
-    #ifdef HTPA32x32dL5_0HiGe
-	 	#define TABLENUMBER		79
-		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		512
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif   
-    #endif
-
-    #ifdef HTPA32x32dL5_0HiGeF7_7
-	 	#define TABLENUMBER		92
-		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif   
-    #endif
-	
+#ifdef ReadToFromTable		//choose only one of them!
+				//#define HTPA32x32dR1L1_6HiGe_Gain3k3
+				//#define HTPA32x32dR1L2_1SiF5_0_N2
+				//#define HTPA32x32dL2_1HiSiF5_0_Gain3k3			//is used for SensorRev. 1	
+				//#define HTPA32x32dR1L1k8_0k7HiGe		
+				//#define HTPA32x32dR1L2_1HiSiF5_0_Precise	
+				//#define HTPA32x32dR1L2_1HiSiF5_0_Gain3k3_Extended
+				//#define HTPA32x32dR1L2_85Hi_Gain3k3		
+				//#define HTPA32x32dR1L3_6HiSi_Rev1_Gain3k3
+		#define HTPA32x32dR1L3_6HiSi_Rev1_Gain3k3_TaExtended	//same like the above but with a larger working ambient temperature range
+				//#define HTPA32x32dR1L5_0HiGeF7_7_Gain3k3	
+				//#define HTPA32x32dR1L5_0HiGeF7_7_Gain3k3_TaExtended	//same like the above but with a larger working ambient temperature range
+				//#define HTPA32x32dR1L7_0HiSi_Gain3k3
+		
+			
     #ifdef HTPA32x32dR1L5_0HiGeF7_7_Gain3k3
 	 	#define TABLENUMBER		113
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -195,7 +160,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
-	
+			
     #ifdef HTPA32x32dR1L5_0HiGeF7_7_Gain3k3_TaExtended
 	 	#define TABLENUMBER		113
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -212,22 +177,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif		
-
-    #ifdef HTPA32x32dL1_6HiGe
-	 	#define TABLENUMBER		101	
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471		//possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif   
-    #endif
-	
+		
     #ifdef HTPA32x32dR1L1_6HiGe_Gain3k3
 	 	#define TABLENUMBER		119
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -244,37 +194,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
-
-    #ifdef HTPA32x32dL2_1HiSi
-	 	#define TABLENUMBER		80
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif
-    #endif
-
-    #ifdef HTPA32x32dL2_1HiSiF5_0
-	 	#define TABLENUMBER		96
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif
-    #endif
-	
+		
     #ifdef HTPA32x32dR1L2_1SiF5_0_N2
 	 	#define TABLENUMBER		130
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -291,7 +211,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
-	
+			
     #ifdef HTPA32x32dL2_1HiSiF5_0_Gain3k3
 	 	#define TABLENUMBER		114
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -308,7 +228,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C	//use REF_CAL=2 here. Table does not match, so GlobalGain ist set to 50 % to compensate this.
 		#define SensRv 1				//Sensor Revision is set to 1 (Redesign)
     #endif	
-	
+			
     #ifdef HTPA32x32dR1L2_1HiSiF5_0_Gain3k3_Extended
 	 	#define TABLENUMBER		114
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -325,7 +245,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
- 	
+			
     #ifdef HTPA32x32dR1L2_1HiSiF5_0_Precise
 	 	#define TABLENUMBER		116
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -342,22 +262,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
-
-    #ifdef HTPA32x32dL2_1HiSiF5_0_withSiFilter
-	 	#define TABLENUMBER		97
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif
-    #endif
-	
+			
     #ifdef HTPA32x32dR1L2_85Hi_Gain3k3
 	 	#define TABLENUMBER		127
 		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -374,37 +279,7 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
     #endif	
-
-    #ifdef HTPA32x32dL3_6HiSi
-	 	#define TABLENUMBER		81
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif
-	#endif
-
-    #ifdef HTPA32x32dL3_6HiSi_Rev1
-	 	#define TABLENUMBER		106
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif
-	#endif
-	
+		
     #ifdef HTPA32x32dR1L3_6HiSi_Rev1_Gain3k3
 	 	#define TABLENUMBER		117
 		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -421,22 +296,24 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
 	#endif	
-
-    #ifdef HTPA32x32dL7_0HiSi
-	 	#define TABLENUMBER		107
+			
+  #ifdef HTPA32x32dR1L3_6HiSi_Rev1_Gain3k3_TaExtended
+	 	#define TABLENUMBER		117
 		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
+		#define NROFTAELEMENTS 	12
+		#define NROFADELEMENTS 	1595	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
 		#define TAEQUIDISTANCE	100		//dK
 		#define ADEQUIDISTANCE	64		//dig
 		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
+		#define TABLEOFFSET		1024
 		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
 		#ifdef EQUIADTABLE
 			#undef FLOATTABLE
 		#endif
-	#endif
-	
+		#define MBITTRIMDefault 0x2C
+		#define SensRv 1
+	#endif		
+			
     #ifdef HTPA32x32dR1L7_0HiSi_Gain3k3
 	 	#define TABLENUMBER		118
 		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
@@ -453,53 +330,8 @@ void htpa3232_measure(int fd,int *htpa3232To);
 		#define MBITTRIMDefault 0x2C
 		#define SensRv 1
 	#endif	
-
-    #ifdef HTPA32x32dL2_1HiSiDLC
-		#define TABLENUMBER		83
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	64		//dig
-		#define ADEXPBITS		6		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		512
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE 
-			#undef FLOATTABLE
-		#endif
-    #endif
-
-    #ifdef HTPA32x32dL2_1Si_withSiFilter
-		#define TABLENUMBER		88
-		#define PCSCALEVAL		100000000		//PixelConst scale value for table
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	471
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	8		//dig
-		#define ADEXPBITS		3		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		64
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE 
-			#undef FLOATTABLE
-		#endif
-    #endif
-
-   #ifdef HTPA32x32dL5_0HiGeMult
-	 	#define TABLENUMBER0		79
-		#define PCSCALEVAL		100000000 //327000000000		//PixelConst scale value for table... lower 'L' for (long)
-		#define NROFTAELEMENTS 	7
-		#define NROFADELEMENTS 	251	//130 possible due to Program memory, higher values possible if NROFTAELEMENTS is decreased
-		#define TAEQUIDISTANCE	100		//dK
-		#define ADEQUIDISTANCE	128		//dig
-		#define ADEXPBITS		7		//2^ADEXPBITS=ADEQUIDISTANCE
-		#define TABLEOFFSET		256
-		#define EQUIADTABLE		//if defined, ADELEMENTS have to be 2^N quantizied! else more CPU Power is needed
-		#ifdef EQUIADTABLE
-			#undef FLOATTABLE
-		#endif   
-    #endif
-	
-    #ifdef HTPA32x32dR1L1k8_0k7HiGe_Bodo
+			
+    #ifdef HTPA32x32dR1L1k8_0k7HiGe
 	 	#define TABLENUMBER		115	
 		#define PCSCALEVAL		100000000		//PixelConst scale value for table... lower 'L' for (long)
 		#define NROFTAELEMENTS 	10
@@ -517,6 +349,5 @@ void htpa3232_measure(int fd,int *htpa3232To);
     #endif	
 #endif
 #endif
-
-
+		
 
