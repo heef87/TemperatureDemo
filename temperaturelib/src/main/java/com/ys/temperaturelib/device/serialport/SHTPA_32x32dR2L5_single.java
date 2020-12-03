5a0ef575c32f3db459323629ece2535a074402b4
heef87 <heef159@163.com>
2020-12-03 10:52:46 +0800

    更新SHTPA_32x32dR2L5_single模块

--- temperaturelib/src/main/java/com/ys/temperaturelib/device/serialport/SHTPA_32x32dR2L5_single.java
+++ temperaturelib/src/main/java/com/ys/temperaturelib/device/serialport/SHTPA_32x32dR2L5_single.java
@@ -1,5 +1,8 @@
 package com.ys.temperaturelib.device.serialport;
 
+import android.util.Log;
+
+import com.ys.temperaturelib.device.TemperatureStorager;
 import com.ys.temperaturelib.temperature.MeasureParm;
 import com.ys.temperaturelib.temperature.TakeTempEntity;
 import com.ys.temperaturelib.temperature.TemperatureEntity;
@@ -33,42 +36,42 @@ public class SHTPA_32x32dR2L5_single extends ProductImp implements TemperaturePa
         TakeTempEntity[] entities = new TakeTempEntity[8];
         TakeTempEntity entity0 = new TakeTempEntity();
         entity0.setDistances(10);
-        entity0.setTakeTemperature(1.15f);//2.3  0  2 -0.8  -0.25
+        entity0.setTakeTemperature(1.15f);
         entities[0] = entity0;
 
         TakeTempEntity entity1 = new TakeTempEntity();
         entity1.setDistances(20);
-        entity1.setTakeTemperature(1.40f);//3.15  -0.05  1.5 -0.8  1.15
+        entity1.setTakeTemperature(1.40f);
         entities[1] = entity1;
 
         TakeTempEntity entity2 = new TakeTempEntity();
         entity2.setDistances(30);
-        entity2.setTakeTemperature(1.85f);//3.7 -0.15  1.2  -0.8  0.3
+        entity2.setTakeTemperature(1.85f);
         entities[2] = entity2;
 
         TakeTempEntity entity3 = new TakeTempEntity();
         entity3.setDistances(40);
-        entity3.setTakeTemperature(2.00f);//4.25  -0.05  1 -0.6  0.1
+        entity3.setTakeTemperature(2.00f);
         entities[3] = entity3;
 
         TakeTempEntity entity4 = new TakeTempEntity();
         entity4.setDistances(50);
-        entity4.setTakeTemperature(2.15f);//4.6  -0.2  0.7  -0.6  -0.6
+        entity4.setTakeTemperature(2.15f);
         entities[4] = entity4;
 
         TakeTempEntity entity5 = new TakeTempEntity();
         entity5.setDistances(60);
-        entity5.setTakeTemperature(2.40f);//4.6  -0.2  0.7  -0.6  -0.6
+        entity5.setTakeTemperature(2.40f);
         entities[5] = entity5;
 
         TakeTempEntity entity6 = new TakeTempEntity();
         entity6.setDistances(70);
-        entity6.setTakeTemperature(2.60f);//4.6  -0.2  0.7  -0.6  -0.6
+        entity6.setTakeTemperature(2.60f);
         entities[6] = entity6;
 
         TakeTempEntity entity7 = new TakeTempEntity();
         entity7.setDistances(100);
-        entity7.setTakeTemperature(2.95f);//4.6  -0.2  0.7  -0.6  -0.6
+        entity7.setTakeTemperature(2.95f);
         entities[7] = entity7;
 
         return entities;
@@ -95,14 +98,22 @@ public class SHTPA_32x32dR2L5_single extends ProductImp implements TemperaturePa
     public float check(float value, TemperatureEntity entity) {
         TakeTempEntity takeTempEntity = getTakeTempEntity();
         if (!takeTempEntity.isNeedCheck()) return value;
-        value = value + takeTempEntity.getTakeTemperature();
-        if (value >= 35f && value <= 36f) {
+        float temp = value + takeTempEntity.getTakeTemperature();
+        if (temp >= 35f && temp <= 36f) {
             int index = (int) (Math.random() * teps.length);
-            value = teps[index];
-        } else if (value >= 36f && value <= 36.3f) {
-            value += 0.3f;
+            temp = teps[index];
+        } else if (temp >= 36f && temp <= 36.3f) {
+            temp += 0.3f;
         }
-        return value;
+        TemperatureStorager.getInstance().add("TO:" + getString(temp) + ", 未补偿值:" + getString(value));
+        return temp;
+    }
+
+    private String getString(float value) {
+        if ((value + "").length() < 6)
+            return value + "";
+        else
+            return (value + "").substring(0, 5);
     }
 
     @Override
